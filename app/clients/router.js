063b9b5598e29cd446269e850dfc690e7de97a62
GustavoSierra <gsierra.business@gmail.com>
2021-11-25 16:40:09 +0100

    some changes to the endpoints

--- app/clients/router.js
+++ app/clients/router.js
@@ -5,7 +5,7 @@ const auth = require('../auth.js');
 
 router.get('/:id', auth.checkAdminOrOwn, controller.getUser);
 router.get('/', auth.checkClient, controller.getUsers);
-router.post('/', controller.createUser);
+router.post('/register', controller.createUser);
 router.post('/login', controller.loginUser);
 router.post('/logout', auth.checkClient, controller.logoutUser);
 router.put('/:id', auth.checkClient, controller.updateUser);
