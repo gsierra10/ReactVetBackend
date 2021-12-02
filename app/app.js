85fb683f4c89cf5e9e13b50775fb9e7569408d4c
GustavoSierra <gsierra.business@gmail.com>
2021-12-02 13:13:09 +0100

    added pets controller and pets router

--- app/app.js
+++ app/app.js
@@ -5,13 +5,21 @@ const express = require('express');
 const routerAppoinments = require('./appoinments/router');
 const routerClients = require('./clients/router');
 const routerAdmins = require('./admins/router');
+const routerPets = require('./pets/router')
 const authMiddleWare = require("./auth");
 
 const app = express();
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
 
+    next();
+});
 app.use(express.json());
 app.use("/appoinments", routerAppoinments);
 app.use("/clients", routerClients);
 app.use("/admins", routerAdmins);
+app.use("/pets", routerPets);
 
-app.listen(process.env.PORT, () => console.log('el servido esta en el puerto', process.env.PORT));
+app.listen(process.env.PORT, () => console.log('PORT', process.env.PORT, 'Enabled'));
